75f914b45001acda1dadc1ea35a6ae837bf19c49
LilithAsh7 <lilly.ashbury@gmail.com>
2024-01-08 13:10:54 -0800

    Set it so that pending bugs show on the bug table first.

--- server/bugsQueries.js
+++ server/bugsQueries.js
@@ -12,7 +12,7 @@ const pool = new Pool({
 // API call for getting all data from the bugs table
 const getAllBugs = (request, response) => {
   // Actual sql code  
-  pool.query("SELECT * FROM bugs WHERE status <> 'inactive' ORDER BY project_id ASC, status ASC, bug_type ASC, CASE priority WHEN 'high' THEN 1 WHEN 'medium' THEN 2 WHEN 'low' THEN 3 ELSE 4 END", (error, results) => {
+  pool.query("SELECT * FROM bugs WHERE status <> 'inactive' ORDER BY project_id ASC, CASE WHEN status = 'pending' THEN 0 ELSE 1 END, status ASC, bug_type ASC, CASE priority WHEN 'high' THEN 1 WHEN 'medium' THEN 2 WHEN 'low' THEN 3 ELSE 4 END", (error, results) => {
     // Error handling  
     if (error) {
         throw error
@@ -27,7 +27,7 @@ const getBugsByStatus = (request, response) => {
   
   const bug_status = (request.params.status);
   if (bug_status === 'all'){
-    pool.query("SELECT * FROM bugs WHERE status <> 'inactive' ORDER BY project_id ASC, status ASC, bug_type ASC, CASE priority WHEN 'high' THEN 1 WHEN 'medium' THEN 2 WHEN 'low' THEN 3 ELSE 4 END", (error, results) => {
+    pool.query("SELECT * FROM bugs WHERE status <> 'inactive' ORDER BY project_id ASC, CASE WHEN status = 'pending' THEN 0 ELSE 1 END, status ASC, bug_type ASC, CASE priority WHEN 'high' THEN 1 WHEN 'medium' THEN 2 WHEN 'low' THEN 3 ELSE 4 END", (error, results) => {
       // Error handling  
       if (error) {
           throw error
@@ -37,7 +37,7 @@ const getBugsByStatus = (request, response) => {
     })
   } else {
     // Actual sql code  
-    pool.query("SELECT * FROM bugs WHERE status = $1 ORDER BY project_id ASC, status ASC, bug_type ASC, CASE priority WHEN 'high' THEN 1 WHEN 'medium' THEN 2 WHEN 'low' THEN 3 ELSE 4 END", [bug_status], (error, results) => {
+    pool.query("SELECT * FROM bugs WHERE status = $1 ORDER BY project_id ASC, bug_type ASC, CASE priority WHEN 'high' THEN 1 WHEN 'medium' THEN 2 WHEN 'low' THEN 3 ELSE 4 END", [bug_status], (error, results) => {
       // Error handling  
       if (error) {
           throw error
