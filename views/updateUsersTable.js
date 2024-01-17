36fff3c26ac1857f0fb9cf36bcd98e3bb8155315
LilithAsh7 <lilly.ashbury@gmail.com>
2024-01-17 13:42:22 -0800

    openUserForm now checks the database if the user tries to update a user. If nothing is returned then it just returns without opening the form.

--- views/updateUsersTable.js
+++ views/updateUsersTable.js
@@ -128,7 +128,7 @@ function submitForm() {
   }
 }
 
-function openUserForm(mode){
+async function openUserForm(mode){
   
   if (mode === 'create') {
     var url = 'http://localhost:3000/userForm?mode=create';
@@ -136,8 +136,16 @@ function openUserForm(mode){
     user_id = prompt("Enter the id of the user you would like to update:");
     if (user_id === null || user_id.trim() === '') {
       return;
+    } else {
+      const response = await fetch(`http://localhost:3000/users/${user_id}`)
+      let userData = await response.json();
+      userData = userData[0]
+      if (!userData) {
+        return;
+      } else {
+        var url = `http://localhost:3000/userForm?user_id=${user_id}&mode=update`;
+      }
     }
-    var url = `http://localhost:3000/userForm?user_id=${user_id}&mode=update`;
   }
   
   var newWindow = window.open(url, '_blank', 'scrollbars=yes,resizable=yes,width=400,height=200');
