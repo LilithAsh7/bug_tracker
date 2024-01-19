7f85828943581f49221bb835e6a62430d80af0b0
LilithAsh7 <lilly.ashbury@gmail.com>
2024-01-19 09:14:42 -0800

    Added console.log statement that checkForSqlCharacters has started

--- server/sqlInjectionSecurity.js
+++ server/sqlInjectionSecurity.js
@@ -1,6 +1,7 @@
 const validator = require('validator');
 
 const checkForSqlCharacters = (string) => {
+  console.log("checkForSqlCharacters() in sqlInjectionSecurity.js")
   if (validator.contains(string, ';') || validator.contains(string, "--") || validator.contains(string, "'")) {
     return true;
   } else { return false; }
