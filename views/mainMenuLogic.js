e57c7a1c7118a08f5702213e28ea6fbdb6f04019
LilithAsh7 <lilly.ashbury@gmail.com>
2024-01-22 12:42:37 -0800

    Added button logic to mainMenuLogic.js

--- views/mainMenuLogic.js
+++ views/mainMenuLogic.js
@@ -2,3 +2,15 @@ function openNewTab(route) {
     const url = window.location.origin + route;
     window.open(url, '_blank');
 }
+
+document.getElementById('bugsButton').addEventListener('click', function() {
+    openNewTab('/bugTable');
+});
+
+document.getElementById('projectsButton').addEventListener('click', function() {
+    openNewTab('/projectTable');
+});
+
+document.getElementById('usersButton').addEventListener('click', function() {
+    openNewTab('/userTable');
+});
